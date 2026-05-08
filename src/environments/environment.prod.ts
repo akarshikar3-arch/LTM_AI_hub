9e8aef31d4796d96e613b6a5dc1fd14e82e08045
akarshikar3-arch <akarshikar3@gmail.com>
2026-05-08 18:18:26 +0530

    Fix: correct Azure backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: '/api',
-  agentApiUrl: 'https://ltmai-backend-api.azurewebsites.net',
-  apiUrl: 'https://ltmai-backend-api.azurewebsites.net',
+  agentApiUrl: 'https://ltmai-backend-api-avgza9hgfhfdabg9.southindia-01.azurewebsites.net',
+  apiUrl: 'https://ltmai-backend-api-avgza9hgfhfdabg9.southindia-01.azurewebsites.net',
   githubToken: ''
 };
